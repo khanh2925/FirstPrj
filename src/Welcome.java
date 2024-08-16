3749fbce2496faeaef28847e36925d338f08fe81
First <23641111.khanh@student.iuh.edu.vn>
2024-08-16 23:13:08 +0700

    Commit and Push

--- src/Welcome.java
+++ src/Welcome.java
@@ -1,6 +1,6 @@
 public class Welcome {
     public static void main(String[] args) {
-        System.out.println("Hello Chu Anh Khoi");
-        System.out.println("Hello KTPM19A");
+        System.out.println("Hello Pham Quoc Khanh");
+        System.out.println("Hello KTPM19A i love you");
     }
 }
